b10218261a89512f7082b2d03ccc995a413e2d79
aranzazu.ab <a.bremauntz.aranzazu@gmail.com>
2026-06-17 13:44:19 +0200

    feat: create Sale class and EmptySaleException

---
+++ src/main/java/level01/EmptySaleException.java
@@ -0,0 +1,7 @@
+package level01;
+
+public class EmptySaleException extends Exception {
+    public EmptySaleException (){
+        super("You must first add product to make a sale");
+    }
+}
--- src/main/java/level01/Sale.java
+++ src/main/java/level01/Sale.java
@@ -1,4 +1,27 @@
 package level01;
 
-public class Sale {
+import java.util.ArrayList;
+
+<public class Sale {
+    private ArrayList<Product> products;
+    private double totalPrice;
+
+    public Sale(){
+        this.products = new ArrayList<>();
+        this.totalPrice = 0;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void calculateTotal() throws EmptySaleException {
+        if (products.isEmpty()){
+            throw new EmptySaleException();
+        } else {
+            for(int i = 0; i< products.size(); ++i){
+                totalPrice= totalPrice + products.get(i).getPrice();
+            }
+        }
+    }
 }
